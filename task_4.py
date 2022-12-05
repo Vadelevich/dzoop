ebdb9f911ed93614fd64d31053cc8bbc1c269dc8
Зубайраева Ольга <vadelevich2013@yandex.ru>
2022-12-05 14:19:20 +0300

    add task_4

--- task_4.py
+++ task_4.py
@@ -3,6 +3,9 @@ class DataBase:
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
+        return cls.__instance
+    def __del__(self):
+        DataBase.__instance = None
 
     def __init__(self, user, psw, port):
         self.user = user
@@ -24,5 +27,6 @@ class DataBase:
 
 db = DataBase('root', '1234', 80)
 db2 = DataBase('root2', '5678', 40)
+print(id(db),id(db2))
 print(db is db2)
 # True
